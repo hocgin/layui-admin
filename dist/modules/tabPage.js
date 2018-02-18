ab35a9f8e8d94fa5d72bbb2f5534fd2fecab249d
hocgin <hocgin@gmail.com>
2018-02-18 22:10:27 +0800

    ~ 修复bug + 新增菜单json

--- dist/modules/tabPage.js
+++ dist/modules/tabPage.js
@@ -1,18 +1,26 @@
-layui.define(['jquery', 'element'], (exports) => {
+layui.define(['jquery', 'element', 'laytpl'], (exports) => {
     let $ = layui.$,
         element = layui.element,
+        laytpl = layui.laytpl,
+        INDEX_URL = 'pages/demo.html',
+        MENU_URL = './dist/store/menu.json',
+
+
         MOD_NAME = 'tabPage',
         FILTER_LAYOUT_TAB_PAGES = 'layout-tab-pages',
         THIS = 'layui-this',
         SHOW = 'layui-show',
+        EVENT = 'lay-event',
         LAY_ID = 'lay-id',
+        navView = $('#VIEW_NAV'),
+
+
         TabPage = function () {
             this.config = {};
         };
     /**
      * {
-     *      href: ''
-     *      id: ''
+     *      href: '' // = id
      *      title: ''
      *      allowClose: false
      * }
@@ -23,7 +31,7 @@ layui.define(['jquery', 'element'], (exports) => {
             $titElem = $tabElem.children(TITLE),
             $contElem = $('.layui-tab-content[lay-filter=' + filter + ']'),
             allowClose = eval(options.allowClose || false),
-            li = '<li lay-id="' + (options.id || '') + '" allow-close="' + (allowClose) + '">' + (options.title || 'unnaming') + '</li>',
+            li = '<li lay-id="' + (options.href || '') + '" allow-close="' + (allowClose) + '">' + (options.title || 'unnaming') + '</li>',
             CLOSE = 'layui-tab-close'
         ;
 
@@ -40,7 +48,7 @@ layui.define(['jquery', 'element'], (exports) => {
 
             // content
             $contElem.children('.layui-tab-item').removeClass(SHOW);
-            $contElem.append('<div class="layui-tab-item ' + SHOW + '" lay-id="' + (options.id || '') + '">' + (html || '') + '</div>');
+            $contElem.append('<div class="layui-tab-item ' + SHOW + '" lay-id="' + (options.href || '') + '">' + (html || '') + '</div>');
             element.tabChange(filter, options.href);
 
             // 调整
@@ -55,44 +63,45 @@ layui.define(['jquery', 'element'], (exports) => {
         $.get($item.attr(LAY_ID), function (html) {
             $item.html(html)
         });
+        return this;
     };
     TabPage.prototype.preTabPage = function (filter = FILTER_LAYOUT_TAB_PAGES) {
         let TITLE = '.layui-tab-title',
             $tabElem = $('.layui-tab[lay-filter=' + filter + ']')
         ;
         call.preTabPage.call($tabElem);
-
+        return this;
     };
     TabPage.prototype.nextTabPage = function (filter = FILTER_LAYOUT_TAB_PAGES) {
         let TITLE = '.layui-tab-title',
             $tabElem = $('.layui-tab[lay-filter=' + filter + ']')
         ;
         call.nextTabPage.call($tabElem);
-
+        return this;
     };
     TabPage.prototype.closeOtherTab = function (filter = FILTER_LAYOUT_TAB_PAGES) {
         let $tabElem = $('.layui-tab[lay-filter=' + filter + ']')
         ;
         call.closeOtherTab.call($tabElem);
+        return this;
     };
     TabPage.prototype.closeThisTab = function (filter = FILTER_LAYOUT_TAB_PAGES) {
         let $tabElem = $('.layui-tab[lay-filter=' + filter + ']');
         call.closeThisTab.call($tabElem);
+        return this;
     };
     TabPage.prototype.closeAllTab = function (filter = FILTER_LAYOUT_TAB_PAGES) {
         let TITLE = '.layui-tab-title',
             $tabElem = $('.layui-tab[lay-filter=' + filter + ']')
         ;
         call.closeAllTab.call($tabElem);
+        return this;
     };
     TabPage.prototype.tabChange = function ($tab) {
         call.tabChange.call($tab);
+        return this;
     };
     TabPage.prototype.init = function () {
-        element.tab({
-            headerElem: '.layui-tab[lay-filter="' + FILTER_LAYOUT_TAB_PAGES + '"]>.layui-tab-title>li',
-            bodyElem: '.layui-tab-content[lay-filter="' + FILTER_LAYOUT_TAB_PAGES + '"]>.layui-tab-item'
-        });
         return this;
     };
     let call = {
@@ -117,7 +126,11 @@ layui.define(['jquery', 'element'], (exports) => {
 
             $li.remove();
             item.remove();
-            call.tabAuto.call($parents, 'del')
+            call.tabAuto.call($parents, 'del');
+            layui.event.call(this, 'element', 'tabDelete(' + FILTER_LAYOUT_TAB_PAGES + ')', {
+                elem: $parents
+                , index: index
+            });
         },
         tabClick(e, $liElem, index) {
             let $this = $liElem || $(this), // tab
@@ -219,8 +232,137 @@ layui.define(['jquery', 'element'], (exports) => {
     // layout.tabs
     TabPage.prototype.render = TabPage.prototype.init;
     let tabPage = new TabPage(),
-        dom = $(document)
+        dom = $(document),
+        win = $(window)
     ;
     tabPage.render();
+    win.resize(() => {
+        $('.layout-side-spread-sm').removeClass('layout-side-spread-sm');
+    });
+    win.ready(() => {
+        let EVENT_CLOSE_THIS_TAB = 'closeThisTab',
+            EVENT_CLOSE_ALL_TAB = 'closeAllTab',
+            EVENT_CLOSE_OTHER_TAB = 'closeOtherTab',
+            hash = window.location.hash;
+        tabPage.tabAdd({
+            href: INDEX_URL,
+            title: '<i class="layui-icon layui-icon-home"></i>',
+            allowClose: false
+        });
+        dom.on('click', 'a[data-menu]', (e) => {
+            let $this = $(e.target),
+                href = $this.attr('href'),
+                $clickedTab = $('.layui-tab[lay-filter="' + FILTER_LAYOUT_TAB_PAGES + '"] .layui-tab-title li[lay-id="' + href + '"]')
+            ;
+            if (!href || $clickedTab[0]) {
+                if ($clickedTab[0] && !$clickedTab.hasClass(THIS)) { // 未被点击
+                    element.tabChange(FILTER_LAYOUT_TAB_PAGES, $clickedTab.eq(0).attr(LAY_ID));
+                }
+                return false;
+            }
+            tabPage.tabAdd({
+                href: href,
+                title: $this.text(),
+                allowClose: true
+            });
+            return false;
+        });
+        dom.on('click', '.ext-tabs-control-prev', (e) => {
+            tabPage.preTabPage();
+        });
+        dom.on('click', '.ext-tabs-control-next', (e) => {
+            tabPage.nextTabPage();
+        });
+        element.on('tabDelete(' + FILTER_LAYOUT_TAB_PAGES + ')', (data) => {
+            let $tab = $(data.elem).children('.layui-tab-title').children('.' + THIS).eq(0),
+                href = $tab.attr(LAY_ID),
+                $nav = $('.ext-layout-side a');
+            $nav.filter('[data-menu][href$="' + href + '"]').parent().addClass(THIS)
+                .siblings().removeClass(THIS);
+        });
+        element.on('nav(tabs-option)', (elem) => {
+            let $this = $(elem),
+                event = $this.attr(EVENT);
+            if (event === EVENT_CLOSE_THIS_TAB) {
+                tabPage.closeThisTab();
+            } else if (event === EVENT_CLOSE_ALL_TAB) {
+                tabPage.closeAllTab();
+            } else if (event === EVENT_CLOSE_OTHER_TAB) {
+                tabPage.closeOtherTab();
+            }
+        });
+        element.on('nav(layout-nav-right)', (elem) => {
+            let $this = $(elem);
+            let event = $this.attr(EVENT);
+            if (event === 'about') {
+                layer.open({
+                    title: '版本信息',
+                    area: ['300px'],
+                    offset: 'r',
+                    type: 1,
+                    skin: 'layui-anim layui-anim-rl layui-layer-adminRight', //样式类名
+                    closeBtn: 0, //不显示关闭按钮
+                    anim: -1,
+                    shadeClose: true, //开启遮罩关闭
+                    content: '内容'
+                });
+            }
+        });
+        element.on('nav(layout-nav-tools)', (elem) => {
+            let $this = $(elem);
+            let event = $this.attr('lay-event');
+            if (event === 'shrink') {
+                if ($(window).width() < 992) {
+                    $('.ext-layout-admin').toggleClass('layout-side-spread-sm');
+                } else {
+                    $('.ext-layout-admin').toggleClass('layadmin-side-shrink');
+                }
+            } else if (event === 'refresh') {
+                tabPage.refresh();
+            }
+        });
+        element.on('tab(layout-tab-pages)', (data) => {
+            let $tab = $(data.elem).children('.layui-tab-title').children('li').eq(data.index),
+                href = $tab.attr(LAY_ID),
+                $nav = $('.ext-layout-side a[data-menu]');
+            $nav.parent().removeClass(THIS);
+
+            $nav.filter('[href$="' + href + '"]').parent().addClass(THIS)
+                .parents('li').addClass('layui-nav-itemed');
+            call.tabChange($tab);
+
+            // 更换地址栏URL #href
+            let hash = window.location.hash;
+            if (href === INDEX_URL) {
+                return;
+            }
+            if (/^#.*/.test(hash)) {
+                hash = hash.replace(/^#.*/, '#' + href);
+            } else {
+                hash += '#' + href;
+            }
+            window.location.hash = hash;
+        });
+
+
+        $.get(MENU_URL, (data) => {
+            if (data.code === 200) {
+                laytpl(TPL_NAV.innerHTML).render(data.result, function (html) {
+                    navView.html(html);
+                    element.render('nav');
+                    // 根据 route 打开Tab
+                    if (!!hash) {
+                        let href = hash.replace(/^#/, '');
+                        console.log(href);
+                        if (href === INDEX_URL) {
+                            return;
+                        }
+                        $('a[data-menu][href$="' + href + '"]').trigger('click');
+                    }
+                });
+            }
+
+        });
+    });
     exports(MOD_NAME, tabPage);
 });
